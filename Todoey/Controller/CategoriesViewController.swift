3695c1fd2f0c849ebefc6c14429bdb58f3eb4987
BanLuk <bandachlukasz@gmail.com>
2018-08-24 08:03:40 +0200

    Reimplemented SwipeCellKit (working, without solution for no categories/items)

--- Todoey/Controller/CategoriesViewController.swift
+++ Todoey/Controller/CategoriesViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import SwipeCellKit
 
 class CategoriesViewController: UITableViewController {
 
@@ -16,31 +17,36 @@ class CategoriesViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         loadCategories()
+        tableView.rowHeight = 80
     }
     
     //MARK: Table View DataSource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        if categoryArray?.count == 0 {
-            return 1
-        }
-        
+//        if categoryArray?.count == 0 {
+//            return 1
+//        }
         return categoryArray?.count ?? 1
     }
     
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SwipeTableViewCell
+//        cell.delegate = self
+//        return cell
+//    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! SwipeTableViewCell
         cell.textLabel?.text = categoryArray?[indexPath.row].name ?? "No Categories added yet"
 //        if categoryArray?.count == 0 {
 //            cell.textLabel?.text = "No Categories added yet"
 //        } else {
 //            cell.textLabel?.text = categoryArray?[indexPath.row].name ?? "No Categories added yet"
 //        }
-        
+        cell.delegate = self
         return cell
     }
     
@@ -59,9 +65,7 @@ class CategoriesViewController: UITableViewController {
     }
     
     func loadCategories(){
-
         categoryArray = realm.objects(Category.self)
-        
         self.tableView.reloadData()
     }
     
@@ -105,7 +109,40 @@ class CategoriesViewController: UITableViewController {
     }
 }
 
+//MARK: - Swipe Cell Delegate Methods
 
+extension CategoriesViewController: SwipeTableViewCellDelegate{
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            
+            if let categoryForDelition = self.categoryArray?[indexPath.row]{
+                do{
+                    try self.realm.write {
+                        self.realm.delete(categoryForDelition)
+                    }
+                } catch{
+                    print("deleting error\(error)")
+                }
+                //tableView.reloadData()
+            }
+            
+        }
+        
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon")
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
+    
+}
 
 
 
